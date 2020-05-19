67ef1c834513fe2ae4a4edbf3db9a8225f249db9
Anna Zhang <anna.zhang@yale.edu>
2020-05-18 17:03:17 -0700

    made 404.html file

--- src/App.js
+++ src/App.js
@@ -15,8 +15,26 @@ import Contact from './pages/Contact';
 import Team from './pages/Team';
 
 
+(function (l) {
+	if (l.search) {
+		var q = {};
+		l.search.slice(1).split('&').forEach(function (v) {
+			var a = v.split('=');
+			q[a[0]] = a.slice(1).join('=').replace(/~and~/g, '&');
+		});
+		if (q.p !== undefined) {
+			window.history.replaceState(null, null,
+				l.pathname.slice(0, -1) + (q.p || '') +
+				(q.q ? ('?' + q.q) : '') +
+				l.hash
+			);
+		}
+	}
+}(window.location))
+
 const App = () => (
 	<div class="center-text">
+
 		<meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
 		<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Comfortaa" />
 		<link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet" />
--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { createBrowserHistory } from "history";
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+
+const history = createBrowserHistory();
+
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter history={history}>
     <App />
   </BrowserRouter>,
   document.getElementById('root')
