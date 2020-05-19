9d63036b0c8509e28cb449497c27606c7b96cd44
Anna Zhang <anna.zhang@yale.edu>
2020-05-18 17:04:11 -0700

    Updates

---
+++ precache-manifest.f8f0cc7533b30fd166909bc439730d93.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6f7f55f05f56d0f3a0460ae94b97445",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eee0b74b916d50345c9e",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "cca2dbcc0d5f90e7becd",
+    "url": "/static/css/main.68469d50.chunk.css"
+  },
+  {
+    "revision": "eee0b74b916d50345c9e",
+    "url": "/static/js/2.36ced13a.chunk.js"
+  },
+  {
+    "revision": "56729d980da09961afe8d497a21eec12",
+    "url": "/static/js/2.36ced13a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cca2dbcc0d5f90e7becd",
+    "url": "/static/js/main.b9cd6f54.chunk.js"
+  },
+  {
+    "revision": "6fc0955fad22ef304e6d",
+    "url": "/static/js/runtime-main.65457b84.js"
+  },
+  {
+    "revision": "e102729d10a43801add77d5707afed56",
+    "url": "/static/media/ceid.e102729d.png"
+  },
+  {
+    "revision": "e54d4116feda389dc3ff0a6ed97cd56c",
+    "url": "/static/media/driver.e54d4116.jpeg"
+  },
+  {
+    "revision": "cdea3935fa835fbd9e23165c79d84d88",
+    "url": "/static/media/logo-blue.cdea3935.png"
+  },
+  {
+    "revision": "d5e5d2c0b393c8510d6f88b487793a78",
+    "url": "/static/media/medicine.d5e5d2c0.jpeg"
+  },
+  {
+    "revision": "4355181d9e91667ea5d2c37cdaaece7c",
+    "url": "/static/media/patients.4355181d.jpeg"
+  },
+  {
+    "revision": "ac231fe241140e2bbaaf9f329abbbdee",
+    "url": "/static/media/pharmacies.ac231fe2.jpeg"
+  },
+  {
+    "revision": "d8baeb5dc7ef1ac14a37e9dd173af060",
+    "url": "/static/media/pharmacy.d8baeb5d.jpeg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.403a44ba8cbc877be70c81b2be7c0e40.js"
+  "/precache-manifest.f8f0cc7533b30fd166909bc439730d93.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
