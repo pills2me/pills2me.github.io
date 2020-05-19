b82ab9a05979205221243132ac21528c36f109cd
Anna Zhang <anna.zhang@yale.edu>
2020-05-18 17:14:33 -0700

    update 404.html

--- src/App.js
+++ src/App.js
@@ -15,23 +15,6 @@ import Contact from './pages/Contact';
 import Team from './pages/Team';
 
 
-(function (l) {
-	if (l.search) {
-		var q = {};
-		l.search.slice(1).split('&').forEach(function (v) {
-			var a = v.split('=');
-			q[a[0]] = a.slice(1).join('=').replace(/~and~/g, '&');
-		});
-		if (q.p !== undefined) {
-			window.history.replaceState(null, null,
-				l.pathname.slice(0, -1) + (q.p || '') +
-				(q.q ? ('?' + q.q) : '') +
-				l.hash
-			);
-		}
-	}
-}(window.location))
-
 const App = () => (
 	<div class="center-text">
 
