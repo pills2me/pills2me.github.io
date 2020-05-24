065120abdafd0ad9fd6d754c4fbf8360d6d155bd
Anna Zhang <anna.zhang@yale.edu>
2020-05-24 11:55:41 -0700

    added general section on homescreen

--- src/pages/OpeningPage.js
+++ src/pages/OpeningPage.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import "../style.css";
 
 import Header from '../components/Header';
-import Footer from '../components/Footer';
+import DetailedFooter from '../components/DetailedFooter';
 
 import Patient from '../images/patient.png';
 import Driver from '../images/driver.png';
@@ -17,35 +17,42 @@ const OpeningPage = () => (
     <div>
         <Header />
         <div class="pharm-background container center column-dir after-heading">
-            <h2 class="logo">Free same-day medication delivery for the elderly and vulnerable during COVID-19</h2>
+            <div class="homescreen center container column-dir">
+                <h2 class="logo">Free same-day medication delivery for the elderly and vulnerable during COVID-19</h2>
 
-            <div class="extra-margin">
-                <div class="container space">
-                    <Button variant="" onClick={() => window.scrollTo(0, 0)}><Link to="/patient-form">
-                        <div class="round-button container center column-dir">
-                            <img class="logo" src={Patient}></img>
-                            Request Delivery
+                <div class="extra-margin">
+                    <div class="container space">
+                        <Button variant="" onClick={() => window.scrollTo(0, 0)}><Link to="/patient-form">
+                            <div class="round-button container center column-dir">
+                                <img class="logo" src={Patient}></img>
+                                Request Delivery
+        </div>
+                        </Link></Button>
+
+                        <Button variant="" onClick={buttonClickHandler}><Link to="/driver-form">
+                            <div class="round-button container center column-dir">
+                                <img class="logo" src={Driver}></img>
+                                Volunteer to Drive
+        </div>
+                        </Link></Button>
+
+
                     </div>
-                    </Link></Button>
+                    <div class="container space">
+                        <h3 class="button-labels">You can also call our toll-free number (800)-475-7488 to make the prescription pickup request.</h3>
 
-                    <Button variant="" onClick={buttonClickHandler}><Link to="/driver-form">
-                        <div class="round-button container center column-dir">
-                            <img class="logo" src={Driver}></img>
-                            Volunteer to Drive
+                        <h3 class="button-labels">Have free time to help deliver medication to patients?</h3>
+
                     </div>
-                    </Link></Button>
-
-
-                </div>
-                <div class="container space">
-                    <h3 class="button-labels">You can also call our toll-free number (800)-475-7488 to make the prescription pickup request.</h3>
-
-                    <h3 class="button-labels">Have free time to help deliver medication to patients?</h3>
-
                 </div>
             </div>
 
-            <Footer />
+
+            <div class="section section-blue">
+                <h1>What We Offer</h1>
+            </div>
+
+            <DetailedFooter />
         </div>
 
 
