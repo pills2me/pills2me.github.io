21d29125c36c13cf1f6ae3d6debf2de263c20f2d
Anna Zhang <anna.zhang@yale.edu>
2020-05-29 11:53:46 -0700

    connected contact form to spreadsheet

--- src/components/DetailedFooter.js
+++ src/components/DetailedFooter.js
@@ -1,44 +1,74 @@
-import React, { Component } from "react";
-import { Button, Card, Form, Jumbotron, Col, Row } from 'react-bootstrap'
-import "../style.css";
+import React, { Component } from 'react';
+import { Button, Card, Form, Jumbotron, Col, Row } from 'react-bootstrap';
+import '../style.css';
+import { Link } from 'react-router-dom';
 
 class Footer extends Component {
-    render() {
-        return (
-            <div class="container detailed-footer">
-                <Row className="container between">
-                    <Col md={3} className="container column-dir center">
-                        <h4>CONTACT US</h4>
-                        <div class="">
-                            <p>Email: <a class="section-link" href="mailto:info@pills2me.com">info@pills2me.com</a> </p>
-                            <p>Toll-free Number: 800-475-7488</p>
-                        </div>
-
-                    </Col>
-                    <Col md={6} >
-                        <h4>CONNECT AND FOLLOW</h4>
-                        <div class="container space">
-                            <a href="https://www.facebook.com/pills2me/" target="_blank" style={{ fontSize: "40px" }} class="container center fa fa-facebook"></a>
-                            <a href="https://twitter.com/" target="_blank" style={{ fontSize: "40px" }} class="container center fa fa-twitter"></a>
-                            <a href="https://www.instagram.com/pills2me/" target="_blank" style={{ fontSize: "40px" }} class="container center fa fa-instagram"></a>
-                            <a href="https://www.linkedin.com/company/pills2me-inc/" target="_blank" style={{ fontSize: "40px" }} class="container center fa fa-linkedin"></a>
-                        </div>
-                        <p>Copyright © Pills2Me. All rights reserved.</p>
-                    </Col>
-                    <Col md={3} >
-                        <h4>JOIN TODAY</h4>
-                        <div>
-                            <p><a class="section-link" href="/patient-form">Request Rx Pickup/Delivery</a></p>
-                            <p><a class="section-link" href="/driver-form">Volunteer to Drive</a></p>
-                        </div>
-                    </Col>
-                </Row>
-            </div >
-        );
-    }
+	render() {
+		return (
+			<div class="container detailed-footer">
+				<Row className="container between">
+					<Col md={3} className="container column-dir center">
+						<h4>CONTACT US</h4>
+						<div class="">
+							<p>
+								Email:{' '}
+								<a class="section-link" href="mailto:info@pills2me.com">
+									info@pills2me.com
+								</a>{' '}
+							</p>
+							<p>Toll-free Number: 800-475-7488</p>
+						</div>
+					</Col>
+					<Col md={6}>
+						<h4>CONNECT AND FOLLOW</h4>
+						<div class="container space">
+							<a
+								href="https://www.facebook.com/pills2me/"
+								target="_blank"
+								style={{ fontSize: '40px' }}
+								class="container center fa fa-facebook"
+							/>
+							<a
+								href="https://twitter.com/"
+								target="_blank"
+								style={{ fontSize: '40px' }}
+								class="container center fa fa-twitter"
+							/>
+							<a
+								href="https://www.instagram.com/pills2me/"
+								target="_blank"
+								style={{ fontSize: '40px' }}
+								class="container center fa fa-instagram"
+							/>
+							<a
+								href="https://www.linkedin.com/company/pills2me-inc/"
+								target="_blank"
+								style={{ fontSize: '40px' }}
+								class="container center fa fa-linkedin"
+							/>
+						</div>
+						<p>Copyright © Pills2Me. All rights reserved.</p>
+					</Col>
+					<Col md={3}>
+						<h4>JOIN TODAY</h4>
+						<div>
+							<p>
+								<Link className="section-link" onClick={() => window.scrollTo(0, 0)} to="/patient-form">
+									Request Rx Pickup/Delivery
+								</Link>
+							</p>
+							<p>
+								<Link className="section-link" onClick={() => window.scrollTo(0, 0)} to="/driver-form">
+									Volunteer to Drive
+								</Link>
+							</p>
+						</div>
+					</Col>
+				</Row>
+			</div>
+		);
+	}
 }
-
-
-
 
 export default Footer;
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { Button, Col, Row, Jumbotron, Form } from 'react-bootstrap'
+import React, { useState } from 'react';
+import { Button, Col, Row, Jumbotron, Form } from 'react-bootstrap';
 import { Redirect } from 'react-router-dom';
 import emailjs from 'emailjs-com';
 import '../style.css';
@@ -7,95 +7,108 @@ import '../style.css';
 import Header from '../components/Header';
 import DetailedFooter from '../components/DetailedFooter';
 
-
-
-
 function Contact() {
+	const [ redirect, setRedirect ] = useState(false);
+	const [ validated, setValidated ] = useState(false);
 
-    const [redirect, setRedirect] = useState(false);
-    const [validated, setValidated] = useState(false);
+	const sendEmail = (e) => {
+		const form = e.currentTarget;
+		if (form.checkValidity() === false) {
+			e.stopPropagation();
+			e.preventDefault();
+		} else {
+			//setRedirect(true);
+		}
+		setValidated(true);
 
-    const sendEmail = (e) => {
-        const form = e.currentTarget;
-        if (form.checkValidity() === false) {
-            e.stopPropagation();
-            e.preventDefault();
-        } else {
-            //setRedirect(true);
-        }
-        setValidated(true);
+		// emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', e.target, 'YOUR_USER_ID')
+		//     .then((result) => {
+		//         console.log(result.text);
+		//         setRedirect(true);
+		//     }, (error) => {
+		//         console.log(error.text);
 
-        e.preventDefault();
+		//     });
+	};
+	if (redirect) {
+		return <Redirect to="/submission-complete" />;
+	}
 
-        emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', e.target, 'YOUR_USER_ID')
-            .then((result) => {
-                console.log(result.text);
-                setRedirect(true);
-            }, (error) => {
-                console.log(error.text);
+	return (
+		<div>
+			<Header />
 
-            });
-    }
-    if (redirect) {
-        return <Redirect to="/submission-complete" />;
-    }
+			<div class="pharm-background fixed-background container center column-dir after-heading">
+				<Row className="my-container" style={{ paddingLeft: '5%', paddingRight: '5%' }}>
+					<Col md={6} className="container center column-dir">
+						<h1>
+							Please feel free to contact us with any questions or tell us how we can better serve you.
+						</h1>
+						<div class="extra-margin">
+							<h6>Phone: (405) 436-0220</h6>
+							<h6>Email: info@pills2me.com</h6>
+						</div>
+					</Col>
+					<Col>
+						<Jumbotron style={{ width: '100%' }}>
+							<Form
+								action="https://docs.google.com/forms/u/0/d/e/1FAIpQLScr-6XtvqMRLVhmVq3N_1bFh3YFXafZpa6BY9UPWw1g4UIsoA/formResponse"
+								noValidate
+								validated={validated}
+								onSubmit={sendEmail}
+							>
+								<Form.Row>
+									<Form.Group as={Col} controlId="firstName">
+										<Form.Label>First Name</Form.Label>
+										<Form.Control
+											required
+											type="name"
+											name="entry.737227013"
+											placeholder="First Name"
+										/>
+									</Form.Group>
+									<Form.Group as={Col} controlId="lastName">
+										<Form.Label>Last Name</Form.Label>
+										<Form.Control
+											required
+											name="entry.1761248022"
+											type="text"
+											placeholder="Last Name"
+										/>
+									</Form.Group>
+								</Form.Row>
 
-    return (
-        <div>
-            <Header />
+								<Form.Group controlId="email">
+									<Form.Label>Email</Form.Label>
+									<Form.Control required name="entry.1868634018" type="email" placeholder="Email" />
+								</Form.Group>
 
-            <div class="pharm-background fixed-background container center column-dir after-heading">
-                <Row className="my-container" style={{ paddingLeft: "5%", paddingRight: "5%" }}>
-                    <Col md={6} className="container center column-dir">
-                        <h1>Please feel free to contact us with any questions or tell us how we can better serve you.</h1>
-                        <div class="extra-margin">
-                            <h6>Phone: (405) 436-0220</h6>
-                            <h6>Email: info@pills2me.com</h6>
-                        </div>
-
-                    </Col>
-                    <Col>
-                        <Jumbotron style={{ width: "100%" }}>
-                            <Form noValidate validated={validated} onSubmit={sendEmail}>
-                                <Form.Row>
-                                    <Form.Group as={Col} controlId="firstName">
-                                        <Form.Label>First Name</Form.Label>
-                                        <Form.Control required type="name" placeholder="First Name" />
-                                    </Form.Group>
-                                    <Form.Group as={Col} controlId="lastName">
-                                        <Form.Label>Last Name</Form.Label>
-                                        <Form.Control required type="text" placeholder="Last Name" />
-                                    </Form.Group>
-                                </Form.Row>
-
-                                <Form.Group controlId="email">
-                                    <Form.Label>Email</Form.Label>
-                                    <Form.Control required type="email" placeholder="Email" />
-                                </Form.Group>
-
-                                <Form.Group controlId="phone">
-                                    <Form.Label>Phone Number</Form.Label>
-                                    <Form.Control required type="tel" placeholder="(###) ###-####" />
-                                </Form.Group>
-                                <Form.Group>
-                                    <Form.Label>Message</Form.Label>
-                                    <Form.Control required as="textarea" rows="3"></Form.Control>
-                                </Form.Group>
-                                <Button variant="light" type="submit">
-                                    SUBMIT
-                                    {/* <Link to="/submission-complete">Submit</Link> */}
-                                </Button>
-                            </Form>
-                        </Jumbotron>
-                    </Col>
-                </Row>
-                <hr class="colored-line" />
-                <DetailedFooter />
-            </div>
-        </div>
-
-    );
+								<Form.Group controlId="phone">
+									<Form.Label>Phone Number</Form.Label>
+									<Form.Control
+										required
+										name="entry.50999402"
+										type="tel"
+										placeholder="(###) ###-####"
+									/>
+								</Form.Group>
+								<Form.Group>
+									<Form.Label>Message</Form.Label>
+									<Form.Control required name="entry.507562908" as="textarea" rows="3" />
+								</Form.Group>
+								<Button variant="light" type="submit">
+									SUBMIT
+									{/* <Link to="/submission-complete">Submit</Link> */}
+								</Button>
+							</Form>
+						</Jumbotron>
+					</Col>
+				</Row>
+				<hr class="colored-line" />
+				<DetailedFooter />
+			</div>
+		</div>
+	);
 }
-
 
 export default Contact;
