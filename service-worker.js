950f8b2df5a48603016489985e48e5b9d0b163a8
Anna Zhang <anna.zhang@yale.edu>
2020-05-29 11:54:59 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f4012453f1876d19716e6781d346c2a0.js"
+  "/precache-manifest.1c18ba475dd08ce9e3953a96e8e4a27e.js"
 );
 
 self.addEventListener('message', (event) => {
