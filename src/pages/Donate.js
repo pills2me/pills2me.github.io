2db33b93c3946c9c17b7c7284a9060b8e5a33343
Anna Zhang <anna.zhang@yale.edu>
2020-06-09 21:11:58 -0700

    updated UI of donations page

--- src/pages/Donate.js
+++ src/pages/Donate.js
@@ -23,34 +23,33 @@ function Donate() {
 			<div className="pharm-background background-donate fixed-background container center column-dir after-heading">
 				<div className="overlay-text-box container center column-dir">
 					<h1 className="overlay-text">Make a Donation</h1>
-					<h4 className="extra-margin">
-						Pills2Me is currently run entirely by volunteers. Please feel free to make a donation to help us
-						provide our volunteer drivers with gas and supplies.{' '}
+					<h4 className="overlay-description">
+						We use the donations we receive to better serve our communities and help the elderly and
+						vulnerable receive their medication faster. We greatly appreciate your help in supporting our
+						mission.
 					</h4>
-					<h4>Any amount will go a long way. We greatly appreciate your support. Thank you.</h4>
-					<a className="logo" href="#donate">
+					{/* <a className="logo" href="#donate">
 						<Button variant="light" className="shadow extra-margin">
 							MAKE A DONATION
 						</Button>
-					</a>
-					<br id="donate" />
+					</a> */}
 				</div>
 
 				<div className="section ">
-					<h1 className="comfortaa">Contribute to our cause</h1>
-					<Row style={{ marginTop: '20px' }}>
-						<Col lg={6} className="my-container center column-dir">
-							<p style={{ marginTop: '0px', width: '70%' }}>
-								Thank you so much for contributing! We use the donations we receive to better serve our
-								communities and help improve the overal user experience. We greatly appreciate your help
-								in supporting our mission.
+					<h1 className="section-heading">Contribute to Our Cause</h1>
+					<Row style={{ marginTop: '40px' }}>
+						<Col xl={6} className="my-container center column-dir">
+							<p style={{ marginTop: '0px' }} className="section-text center-text">
+								Pills2Me is currently run entirely by volunteers. Please feel free to make a donation to
+								help us provide our volunteer drivers with gas and supplies. Any amount will go a long
+								way. We greatly appreciate your support. Thank you.
 							</p>
 							<img src={donateimg} className="donate-img" />
 						</Col>
 						<Col className="container center">
 							<Jumbotron
 								className="container column-dir space"
-								style={{ width: '80%', minHeight: '100%' }}
+								style={{ width: '80%', minHeight: '90%', marginTop: '0px' }}
 							>
 								{donationAmount == 0.01 ? (
 									<div className="center-text container column-dir space">
@@ -59,25 +58,9 @@ function Donate() {
 											<Button
 												className="donation-btn"
 												variant="light"
-												onClick={() => setDonationAmount(5.0)}
-											>
-												$5
-											</Button>
-											<Button
-												className="donation-btn"
-												variant="light"
 												onClick={() => setDonationAmount(10.0)}
 											>
 												$10
-											</Button>
-										</div>
-										<div>
-											<Button
-												className="donation-btn"
-												variant="light"
-												onClick={() => setDonationAmount(15.0)}
-											>
-												$15
 											</Button>
 											<Button
 												className="donation-btn"
@@ -85,6 +68,22 @@ function Donate() {
 												onClick={() => setDonationAmount(20.0)}
 											>
 												$20
+											</Button>
+										</div>
+										<div>
+											<Button
+												className="donation-btn"
+												variant="light"
+												onClick={() => setDonationAmount(30.0)}
+											>
+												$30
+											</Button>
+											<Button
+												className="donation-btn"
+												variant="light"
+												onClick={() => setDonationAmount(50.0)}
+											>
+												$50
 											</Button>
 										</div>
 
@@ -106,13 +105,22 @@ function Donate() {
 												<FormControl
 													id="inlineFormInputGroup"
 													onChange={otherHandler}
-													placeholder="(e.g. 100, 500)"
+													placeholder="(e.g. 100)"
 												/>
 											</InputGroup>
 										</Button>
 									</div>
 								) : (
-									<Paypal donation={donationAmount} />
+									<div>
+										<Paypal donation={donationAmount} />
+										<Button
+											variant="light"
+											className="back-btn"
+											onClick={() => setDonationAmount(0.01)}
+										>
+											CANCEL
+										</Button>
+									</div>
 								)}
 							</Jumbotron>
 						</Col>
