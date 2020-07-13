988db292ce9b88a499b4538cb3cc57219f5d4e48
Anna Zhang <anna.zhang@yale.edu>
2020-07-12 20:03:32 -0700

    fixed cookies

--- src/components/Cookies.js
+++ src/components/Cookies.js
@@ -3,6 +3,24 @@ import Popup from 'reactjs-popup';
 import { Button } from 'react-bootstrap';
 import { NONAME } from 'dns';
 
+!(function(f, b, e, v, n, t, s) {
+	if (f.fbq) return;
+	n = f.fbq = function() {
+		n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments);
+	};
+	if (!f._fbq) f._fbq = n;
+	n.push = n;
+	n.loaded = !0;
+	n.version = '2.0';
+	n.queue = [];
+	t = b.createElement(e);
+	t.async = !0;
+	t.src = v;
+	s = b.getElementsByTagName(e)[0];
+	s.parentNode.insertBefore(t, s);
+})(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '676650016509645');
+
 class Cookies extends React.Component {
 	constructor(props) {
 		super(props);
@@ -10,11 +28,17 @@ class Cookies extends React.Component {
 		this.openModal = this.openModal.bind(this);
 		this.closeModal = this.closeModal.bind(this);
 	}
+
 	openModal() {
 		this.setState({ open: true });
 	}
 	closeModal() {
 		this.setState({ open: false });
+		this.acceptedCookies();
+	}
+
+	acceptedCookies() {
+		fbq('track', 'PageView');
 	}
 
 	render() {
