6ce8534f6a65ef3a1a7c3b4cff7c016d112c7bf3
Anna Zhang <anna.zhang@yale.edu>
2020-08-19 12:32:56 -0700

    Updated form to fix payment

--- src/pages/PatientForm.js
+++ src/pages/PatientForm.js
@@ -129,12 +129,27 @@ class PatientForm extends Component {
   };
   immunoCheckHandler = event => {
     this.setState({ immunoCheck: !this.state.immunoCheck });
+    this.calculateCost(
+      this.state.copayAmt,
+      !this.state.immunoCheck,
+      this.state.tipAmt
+    );
   };
   copayAmtHandler = event => {
     this.setState({ copayAmt: event.target.value });
+    this.calculateCost(
+      event.target.value,
+      this.state.immunoCheck,
+      this.state.tipAmt
+    );
   };
   tipAmtHandler = event => {
     this.setState({ tipAmt: event.target.value });
+    this.calculateCost(
+      this.state.copayAmt,
+      this.state.immunoCheck,
+      event.target.value
+    );
   };
 
   handleSubmit(e) {
@@ -255,10 +270,6 @@ class PatientForm extends Component {
     }
   }
 
-  completePaymentHandler() {
-    this.setState({ paymentComplete: true });
-  }
-
   paypalHandler() {
     return (
       <div className="container column-dir center">
@@ -274,47 +285,26 @@ class PatientForm extends Component {
     );
   }
 
-  calculateCost() {
+  calculateCost = (copay, immunocheck, tip) => {
     let total = 0;
-    if (!this.state.immunoCheck) {
+    if (!immunocheck) {
       console.log("delivery fee");
       total += DELIVERY_FEE;
     }
-    total += Number(this.state.copayAmt);
-    total += Number(this.state.tipAmt);
+    if (copay) {
+      total += Number(copay);
+    }
+    if (tip) {
+      total += Number(tip);
+    }
+    total = total.toFixed(2);
     console.log("Total: " + total);
     this.setState({
       paymentsEntered: true,
       paymentsComplete: false,
       totalCost: total
     });
-  }
-
-  goToPayment() {
-    console.log(this.state.formSelector);
-
-    if (this.state.formSelector != "") {
-      let deliveryFee = 0;
-      let total = 0;
-      if (!this.state.immunoCheck) {
-        deliveryFee = DELIVERY_FEE;
-      }
-      total =
-        Number(this.state.copayAmt) +
-        Number(this.state.tipAmt) +
-        deliveryFee +
-        0;
-      if (total == 0) {
-        this.setState({ paymentsEntered: true });
-      } else {
-        this.setState({
-          paymentsEntered: true,
-          paymentsComplete: false,
-          totalCost: total
-        });
-      }
-    }
-  }
+  };
 
   pageOne() {
     return (
@@ -349,56 +339,13 @@ class PatientForm extends Component {
     );
   }
 
-  pageTwo() {
-    return (
-      <div className="container column-dir center">
-        <div className="container flex-start">
-          <Button
-            variant="light"
-            onClick={() =>
-              this.setState({
-                paymentsComplete: true,
-                paymentsEntered: false,
-                formSelector: ""
-              })
-            }
-          >
-            Back
-          </Button>
-        </div>
-        <div className="container column-dir center">
-          <h4 className="center-text">
-            Please select a payment method for your payment of $
-            {this.state.totalCost}
-          </h4>
-          <Paypal
-            donation={this.state.totalCost}
-            message="Thank you for your payment"
-          />
-        </div>
-      </div>
-    );
-  }
-
   formHandler() {
     if (this.state.formSelector == "") {
       return <div>{this.pageOne()}</div>;
-    } else if (this.state.formSelector == nvform) {
-      return <div>{this.nvForm()}</div>;
     } else {
       return <div>{this.otherForm()}</div>;
     }
   }
-
-  //   thanks() {
-  //     return (
-  //       <div className="container column-dir center">
-  //         <h3 className="center-text form-subheading">
-  //           Thanks for using Pills2Me!
-  //         </h3>
-  //       </div>
-  //     );
-  //   }
 
   otherForm() {
     return (
@@ -409,7 +356,11 @@ class PatientForm extends Component {
             onClick={() =>
               this.setState({
                 paymentsEntered: false,
-                formSelector: ""
+                formSelector: "",
+                copayAmt: 0,
+                copay: false,
+                immunoCheck: true,
+                tipAmt: 0
               })
             }
           >
@@ -595,13 +546,6 @@ class PatientForm extends Component {
             />
           </Form.Group>
         </Form.Row>
-        {/* 
-            <Form.Group controlId="immunoOld">
-            <Form.Label>Please select an option: </Form.Label>
-                <Form.Check required  checked={this.state.immunoCheck} onClick={() => this.setState({ immunoCheck: true })} label="I am immunocompromised, or I am older than 65 yrs old" type="radio" />
-                <Form.Check required  checked={!this.state.immunoCheck} onClick={() =>this.setState({ immunoCheck: false})} label="I am NOT immunocompromised, and I am younger than 65 yrs old" type="radio" />
-                {this.copayPaymentHandler()}
-            </Form.Group> */}
 
         <Form.Group controlId="notes">
           <Form.Label>Additional Notes</Form.Label>
@@ -649,6 +593,34 @@ class PatientForm extends Component {
             />
             {this.copayPaymentHandler()}
           </div>
+          {this.state.formSelector == nvform ? (
+            <div>
+              <Form.Label>
+                Are you over 65 years old or immunocompromised?
+              </Form.Label>
+              <div>
+                <Form.Check
+                  required
+                  inline
+                  checked={this.state.immunoCheck}
+                  onClick={this.immunoCheckHandler}
+                  label="Yes"
+                  type="radio"
+                />
+                <Form.Check
+                  required
+                  inline
+                  checked={!this.state.immunoCheck}
+                  onClick={this.immunoCheckHandler}
+                  label="No"
+                  type="radio"
+                />
+              </div>
+            </div>
+          ) : (
+            <></>
+          )}
+
           <Form.Label>How much would you like to tip your driver?</Form.Label>
           <InputGroup className="mb-2">
             <InputGroup.Prepend>
@@ -658,27 +630,25 @@ class PatientForm extends Component {
               name="entry.712556072"
               type="text"
               placeholder="ex. $2, $5, 0"
-              onChange={() => {
-                this.tipAmtHandler();
-              }}
+              onChange={this.tipAmtHandler}
             />
           </InputGroup>
           {this.state.copay ||
           !this.state.immunoCheck ||
           this.state.tipAmt != 0 ? (
             <div>
-              {this.state.paymentsEntered ? (
+              {/* {this.state.paymentsEntered ? (
                 <div>
                   <Button variant="light" onClick={() => this.calculateCost()}>
                     Refresh payment
-                  </Button>
-                  {this.paypalHandler()}
-                </div>
+                  </Button> */}
+              {this.paypalHandler()}
+              {/* </div>
               ) : (
                 <Button variant="light" onClick={() => this.calculateCost()}>
                   Pay Amount Due
                 </Button>
-              )}
+              )} */}
             </div>
           ) : (
             <div></div>
@@ -701,358 +671,13 @@ class PatientForm extends Component {
             type="text"
           ></Form.Control>
         </Form.Group>
-        {/* {!this.state.paymentsEntered ? <div></div> : this.goToPayment} */}
-        {/* {this.state.paymentsComplete ? <div></div> : this.pageTwo()} */}
 
         <Button variant="light" type="submit">
           SUBMIT
-          {/* <Link to="/submission-complete">SUBMIT</Link> */}
         </Button>
       </div>
     );
   }
-
-  nvForm() {
-    return (
-      <div>
-        <div className="container flex-start">
-          <Button
-            variant="light"
-            onClick={() =>
-              this.setState({
-                paymentsEntered: false,
-                formSelector: ""
-              })
-            }
-          >
-            Back
-          </Button>
-        </div>
-        <h3 className="form-subheading">Personal Information</h3>
-        <Form.Row>
-          <Form.Group as={Col} controlId="firstName">
-            <Form.Label>First Name</Form.Label>
-            <Form.Control
-              name="entry.1148577921"
-              required
-              type="text"
-              placeholder="First name"
-              onChange={this.firstNameHandler}
-            />
-          </Form.Group>
-          <Form.Group as={Col} controlId="lastName">
-            <Form.Label>Last Name</Form.Label>
-            <Form.Control
-              name="entry.2018158473"
-              required
-              onChange={this.lastNameHandler}
-              type="text"
-              placeholder="Last Name"
-            />
-          </Form.Group>
-        </Form.Row>
-
-        <Form.Group controlId="email">
-          <Form.Label>Email</Form.Label>
-          <Form.Control
-            name="entry.2136730629"
-            required
-            onChange={this.emailHandler}
-            type="email"
-            placeholder="Email"
-          />
-        </Form.Group>
-
-        <Form.Group controlId="phone">
-          <Form.Label>Phone Number</Form.Label>
-          <Form.Control
-            name="entry.1588733925"
-            required
-            onChange={this.phoneHandler}
-            type="tel"
-            placeholder="(###) ###-####"
-          />
-        </Form.Group>
-        <Form.Group controlId="birthday">
-          <Form.Label>Birthday</Form.Label>
-          <Form.Control
-            name="entry.1566583663"
-            required
-            onChange={this.birthdayHandler}
-            type="date"
-            placeholder="MM/DD/YYYY"
-          />
-        </Form.Group>
-
-        <Form.Group controlId="address1">
-          <Form.Label>Delivery Address 1</Form.Label>
-          <Form.Control
-            name="entry.2047640905"
-            required
-            onChange={this.streetHandler}
-            type="text"
-            placeholder="ex. 123 Main St."
-          />
-        </Form.Group>
-        <Form.Group controlId="address2">
-          <Form.Control
-            name="entry.105694119"
-            type="text"
-            onChange={this.street2Handler}
-            placeholder="Address 2 (ex. apt #)"
-          />
-        </Form.Group>
-        <Form.Row>
-          <Form.Group as={Col} controlId="city">
-            <Form.Label>City</Form.Label>
-            <Form.Control
-              name="entry.1449622927"
-              required
-              onChange={this.cityHandler}
-              type="text"
-              placeholder="City"
-            />
-          </Form.Group>
-          <Form.Group as={Col} controlId="state">
-            <Form.Label>State/Province</Form.Label>
-            <Form.Control
-              name="entry.2066035813"
-              required
-              onChange={this.stateHandler}
-              type="text"
-              placeholder="State/Province"
-            />
-          </Form.Group>
-          <Form.Group as={Col} controlId="zip">
-            <Form.Label>Zip/Postal Code</Form.Label>
-            <Form.Control
-              name="entry.567153649"
-              required
-              onChange={this.zipHandler}
-              type="text"
-              placeholder="Zip/Postal Code"
-            />
-          </Form.Group>
-        </Form.Row>
-        <h3 className="form-subheading">Prescription Pickup Information</h3>
-
-        <Form.Group controlId="pharmacy">
-          <Form.Label>Pharmacy Name</Form.Label>
-          <Form.Control
-            name="entry.334302613"
-            required
-            onChange={this.pharmHandler}
-            type="text"
-            placeholder="Pharmacy Name"
-          />
-        </Form.Group>
-        <Form.Group controlId="pharmacy-phone">
-          <Form.Label>Pharmacy Phone Number</Form.Label>
-          <Form.Control
-            name="entry.961570699"
-            required
-            onChange={this.pharmPhoneHandler}
-            type="tel"
-            placeholder="(XXX) XXX-XXXX"
-          />
-        </Form.Group>
-
-        <Form.Group controlId="address1">
-          <Form.Label>Pharmacy Address 1</Form.Label>
-          <Form.Control
-            name="entry.1130267675"
-            required
-            onChange={this.pharmStreet1Handler}
-            type="text"
-            placeholder="ex. 123 Main St."
-          />
-        </Form.Group>
-        <Form.Group controlId="address2">
-          <Form.Control
-            name="entry.1454592168"
-            onChange={this.pharmStreet2Handler}
-            type="text"
-            placeholder="Address 2 (ex. apt #)"
-          />
-        </Form.Group>
-        <Form.Row>
-          <Form.Group as={Col} controlId="city">
-            <Form.Label>City</Form.Label>
-            <Form.Control
-              name="entry.1882273497"
-              required
-              onChange={this.pharmCityHandler}
-              type="text"
-              placeholder="City"
-            />
-          </Form.Group>
-          <Form.Group as={Col} controlId="state">
-            <Form.Label>State/Province</Form.Label>
-            <Form.Control
-              name="entry.934622981"
-              required
-              onChange={this.pharmStateHandler}
-              type="text"
-              placeholder="State/Province"
-            />
-          </Form.Group>
-          <Form.Group as={Col} controlId="zip">
-            <Form.Label>Zip/Postal Code</Form.Label>
-            <Form.Control
-              name="entry.501020162"
-              required
-              onChange={this.pharmZipHandler}
-              type="text"
-              placeholder="Zip/Postal Code"
-            />
-          </Form.Group>
-        </Form.Row>
-        {/* 
-            <Form.Group controlId="immunoOld">
-            <Form.Label>Please select an option: </Form.Label>
-                <Form.Check required  checked={this.state.immunoCheck} onClick={() => this.setState({ immunoCheck: true })} label="I am immunocompromised, or I am older than 65 yrs old" type="radio" />
-                <Form.Check required  checked={!this.state.immunoCheck} onClick={() =>this.setState({ immunoCheck: false})} label="I am NOT immunocompromised, and I am younger than 65 yrs old" type="radio" />
-                {this.copayPaymentHandler()}
-            </Form.Group> */}
-
-        <Form.Group controlId="notes">
-          <Form.Label>Additional Notes</Form.Label>
-          <Form.Control
-            name="entry.416985133"
-            as="textarea"
-            onChange={this.notesHandler}
-            rows="3"
-            placeholder="Please provide any additional notes to us that you would like us to know."
-          />
-        </Form.Group>
-
-        <Form.Group controlId="ready">
-          <Form.Label>Is your prescription ready for pickup?</Form.Label>
-          <Form.Check
-            required
-            inline
-            label="Please check this only if your prescription is ready for pickup from your listed pharmacy."
-            type="checkbox"
-          />
-        </Form.Group>
-        <Form.Group>
-          <h3 className="form-subheading">Payment Information</h3>
-          <Form.Label>Do you have a copay?</Form.Label>
-          <div>
-            <Form.Check
-              required
-              inline
-              checked={this.state.copay}
-              onClick={() => {
-                this.setState({ copay: true });
-              }}
-              label="Yes"
-              type="radio"
-            />
-            <Form.Check
-              required
-              inline
-              checked={!this.state.copay}
-              onClick={() => this.setState({ copay: false })}
-              label="No"
-              type="radio"
-            />
-          </div>
-          {this.copayPaymentHandler()}
-          <Form.Label>
-            Are you over 65 years old or immunocompromised?
-          </Form.Label>
-          <div>
-            <Form.Check
-              required
-              inline
-              checked={this.state.immunoCheck}
-              onClick={() => {
-                this.setState({ immunoCheck: true });
-              }}
-              label="Yes"
-              type="radio"
-            />
-            <Form.Check
-              required
-              inline
-              checked={!this.state.immunoCheck}
-              onClick={() => {
-                this.setState({ immunoCheck: false });
-              }}
-              label="No"
-              type="radio"
-            />
-          </div>
-          <Form.Label>How much would you like to tip your driver?</Form.Label>
-          <InputGroup className="mb-2">
-            <InputGroup.Prepend>
-              <InputGroup.Text>$</InputGroup.Text>
-            </InputGroup.Prepend>
-            <Form.Control
-              name="entry.712556072"
-              type="text"
-              placeholder="ex. $2, $5, 0"
-              onChange={() => {
-                this.tipAmtHandler();
-              }}
-            />
-          </InputGroup>
-          {this.state.copay ||
-          !this.state.immunoCheck ||
-          this.state.tipAmt != 0 ? (
-            <div>
-              {this.state.paymentsEntered ? (
-                <div>
-                  <Button variant="light" onClick={() => this.calculateCost()}>
-                    Refresh payment
-                  </Button>
-                  {this.paypalHandler()}
-                </div>
-              ) : (
-                <Button variant="light" onClick={() => this.calculateCost()}>
-                  Pay Amount Due
-                </Button>
-              )}
-            </div>
-          ) : (
-            <div></div>
-          )}
-        </Form.Group>
-        <Form.Group>
-          <h3 className="form-subheading">Disclaimer</h3>
-          <Form.Check
-            required
-            type="checkbox"
-            label="By submitting the form, you are authorizing one of our volunteers to pick up and deliver your medication(s). We will do our best to deliver their medications as fast as we can (same day), but cannot guarantee that in all cases due to circumstances that might arise beyond our control."
-          />
-        </Form.Group>
-        <Form.Group>
-          <Form.Label>How did you hear about us?</Form.Label>
-          <Form.Control
-            name="entry.619637477"
-            required
-            onChange={this.hearHandler}
-            type="text"
-          ></Form.Control>
-        </Form.Group>
-        {/* {!this.state.paymentsEntered ? <div></div> : this.goToPayment} */}
-        {/* {this.state.paymentsComplete ? <div></div> : this.pageTwo()} */}
-        {/* {this.state.copay ||
-        !this.state.immunoCheck ||
-        this.state.tipAmt != 0 ? (
-          <div>{this.paypalHandler()}</div>
-        ) : (
-          <div></div>
-        )} */}
-        <Button variant="light" type="submit">
-          SUBMIT
-          {/* <Link to="/submission-complete">SUBMIT</Link> */}
-        </Button>
-      </div>
-    );
-  }
-  //onSubmit={this.handleSubmit.bind(this)}
 
   render() {
     if (this.state.redirect) {
